51b3682587ff0f9ffe76f34070d94c228cee2886
Tarik Vural <93211813+tarikvural@users.noreply.github.com>
2024-07-23 17:41:44 +0200

    Time: 664 ms (45.61%) | Memory: 0B (100.00%) - LeetSync

---
+++ sql/2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+with success as (
+    select user_id, coalesce(count(*),0) as amount
+    from confirmations
+    where action='confirmed'
+    group by user_id
+), tries as (
+    select user_id, count(*) as amount
+    from confirmations
+    group by user_id
+)
+select signups.user_id, round(coalesce(success.amount/tries.amount,0),2) as confirmation_rate
+from signups 
+left join tries 
+    on signups.user_id = tries.user_id 
+left join success 
+    on tries.user_id = success.user_id
