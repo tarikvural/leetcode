b9fed4138715f76809e28555535764d4bd6e1cdf
Tarik Vural <93211813+tarikvural@users.noreply.github.com>
2024-07-23 23:56:07 +0200

    Time: 81 ms (51.10%) | Memory: 59 MB (97.00%) - LeetSync

---
+++ interview-questions/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,12 @@
+function maxProfit(prices: number[]): number {
+    let maxProfit = 0;
+    let minPrice = Infinity;
+    for(let i = 0; i < prices.length; i++){
+        if(prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+    return maxProfit;
+};
